56dac9f6e11cc8c500a7b25a43d6a92ac09e0ddd
JThompsn <jthomp888999@users.noreply.github.com>
2021-02-16 14:01:07 -0600

    testing removing more unneeded attributes, added attachments, and adding animations, just to see what happens

--- config.cpp
+++ config.cpp
@@ -11,6 +11,7 @@ class CfgPatches
 		};
 	};
 };
+
 class CfgVehicles
 {
 	class Inventory_Base;
@@ -23,16 +24,50 @@ class CfgVehicles
 		model="\dz\structures\furniture\cases\locker\locker_closed_blue_v1.p3d";
 		weight=10000;
 		itemSize[]={5,15};
-		itemBehaviour=0;
-		stackedUnit="ml";
 		physLayer="item_large";
 		allowOwnedCargoManipulation=0;
+
+		attachments[]=
+		{
+			"Head",
+			"Shoulder",
+			"Melee",
+			"Headgear",
+			"Mask",
+			"Eyewear",
+			"Hands",
+			"LeftHand",
+			"Gloves",
+			"Armband",
+			"Vest",
+			"Body",
+			"Back",
+			"Hips",
+			"Legs",
+			"Feet"
+		};
 
         class Cargo
 		{
 			itemsCargoSize[]={10,20};
 			openable=1;
 			allowOwnedCargoManipulation=1;
+		};
+
+		class AnimationSources
+		{
+			class Lid
+			{
+				source="user";
+				initPhase=0;
+				animPeriod=0.0099999998;
+			};
+			class Lid2
+			{
+				source="user";
+				initPhase=1;
+				animPeriod=0.0099999998;
+			};
 		};
     };
 	class Craftable_Locker_Blue: Craftable_Locker_Color_Base
