3b51dcf27557719432ae224367e9636a665c7583
JThompsn <jthomp888999@users.noreply.github.com>
2021-02-24 08:30:29 -0600

    tweaking min/max values for recipe to match working examples

--- scripts/4_world/CraftLocker.c
+++ scripts/4_world/CraftLocker.c
@@ -2,24 +2,24 @@ class CraftLocker extends RecipeBase
 {
 	override void Init()
 	{
-		m_Name = "#STR_CraftLocker0";
+		m_Name = "Craft Locker";
 		m_IsInstaRecipe = false;//should this recipe be performed instantly without animation
 		m_AnimationLength = 1;//animation length in relative time units
-		m_Specialty = -0.01;// value > 0 for roughness, value < 0 for precision
+		m_Specialty = 0.02;// value > 0 for roughness, value < 0 for precision
 		
 		
 		//conditions
 		m_MinDamageIngredient[0] = -1;//-1 = disable check
-		m_MaxDamageIngredient[0] = 10;//-1 = disable check
+		m_MaxDamageIngredient[0] = 3;//-1 = disable check
 		
-		m_MinQuantityIngredient[0] = 10;//-1 = disable check
+		m_MinQuantityIngredient[0] = -1;//-1 = disable check
 		m_MaxQuantityIngredient[0] = -1;//-1 = disable check
 		
 		m_MinDamageIngredient[1] = -1;//-1 = disable check
 		m_MaxDamageIngredient[1] = 3;//-1 = disable check
 		
 		m_MinQuantityIngredient[1] = -1;//-1 = disable check
-		m_MaxQuantityIngredient[1] = 0;//-1 = disable check
+		m_MaxQuantityIngredient[1] = -1;//-1 = disable check
 		//----------------------------------------------------------------------------------------------------------------------
 		
 		//INGREDIENTS
@@ -28,8 +28,8 @@ class CraftLocker extends RecipeBase
 		
 		m_IngredientAddHealth[0] = 0;// 0 = do nothing
 		m_IngredientSetHealth[0] = -1; // -1 = do nothing
-		m_IngredientAddQuantity[0] = -10;// 0 = do nothing
-		m_IngredientDestroy[0] = false;//true = destroy, false = do nothing
+		m_IngredientAddQuantity[0] = -1;// 0 = do nothing
+		m_IngredientDestroy[0] = true;//true = destroy, false = do nothing
 		m_IngredientUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this ingredient
 		
 		//ingredient 2
@@ -46,7 +46,7 @@ class CraftLocker extends RecipeBase
 		AddResult("Craftable_Locker");//add results here
 
 		m_ResultSetFullQuantity[0] = false;//true = set full quantity, false = do nothing
-		m_ResultSetQuantity[0] = -1;//-1 = do nothing
+		m_ResultSetQuantity[0] = 0;//-1 = do nothing
 		m_ResultSetHealth[0] = -1;//-1 = do nothing
 		m_ResultInheritsHealth[0] = -2;// (value) == -1 means do nothing; a (value) >= 0 means this result will inherit health from ingredient number (value);(value) == -2 means this result will inherit health from all ingredients averaged(result_health = combined_health_of_ingredients / number_of_ingredients)
 		m_ResultInheritsColor[0] = -1;// (value) == -1 means do nothing; a (value) >= 0 means this result classname will be a composite of the name provided in AddResult method and config value "color" of ingredient (value)
