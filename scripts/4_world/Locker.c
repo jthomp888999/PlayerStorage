03319c376f03ccabd5490cb10b66f613da114a63
JThompsn <jthomp888999@users.noreply.github.com>
2021-03-18 07:16:50 -0500

    remove on store save since it didn't have anything to do with storage persistence

--- scripts/4_world/Locker.c
+++ scripts/4_world/Locker.c
@@ -7,19 +7,6 @@ class Craftable_Locker: DeployableContainer_Base
         return true; 
     }
 
-    override void OnStoreSave( ParamsWriteContext ctx )
-	{   
-		super.OnStoreSave( ctx );
-		
-		ctx.Write( m_Openable.IsOpened() );
-	}
-	
-	override bool OnStoreLoad( ParamsReadContext ctx, int version )
-	{
-		if ( !super.OnStoreLoad( ctx, version ) )
-			return false;
-	}
-
     override void OnPlacementComplete( Man player, vector position = "0 0 0", vector orientation = "0 0 0" )
     {
         super.OnPlacementComplete( player, position, orientation );
