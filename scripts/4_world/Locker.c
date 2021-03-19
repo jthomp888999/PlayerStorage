f754554a814bd8802dc129b633385a79c48b3a24
JThompsn <jthomp888999@users.noreply.github.com>
2021-03-19 18:13:07 -0500

    fixing attachments

--- scripts/4_world/Locker.c
+++ scripts/4_world/Locker.c
@@ -7,6 +7,11 @@ class Craftable_Locker: DeployableContainer_Base
         return true; 
     }
 
+	override bool CanPutInCargo( EntityAI parent )
+	{
+		return true;
+	}
+
     override void OnPlacementComplete( Man player, vector position = "0 0 0", vector orientation = "0 0 0" )
     {
         super.OnPlacementComplete( player, position, orientation );
@@ -26,4 +31,36 @@ class Craftable_Locker: DeployableContainer_Base
         AddAction(ActionTogglePlaceObject);
         AddAction(ActionPlaceObject);
     }
+
+    override bool CanReceiveAttachment( EntityAI attachment, int slotId )
+	{
+		if ( GetHealthLevel() == GameConstants.STATE_RUINED )
+			return false;
+		
+		return super.CanReceiveAttachment(attachment, slotId);
+	}
+	
+	override bool CanLoadAttachment( EntityAI attachment )
+	{
+		if ( GetHealthLevel() == GameConstants.STATE_RUINED )
+			return false;
+		
+		return super.CanLoadAttachment(attachment);
+	}
+
+	override bool CanReceiveItemIntoCargo( EntityAI item )
+	{
+		if ( GetHealthLevel() == GameConstants.STATE_RUINED )
+			return false;
+
+		return super.CanReceiveItemIntoCargo( item );
+	}
+	
+	override bool CanLoadItemIntoCargo( EntityAI item )
+	{
+		if ( GetHealthLevel() == GameConstants.STATE_RUINED )
+			return false;
+
+		return super.CanLoadItemIntoCargo( item );
+	}
 };
